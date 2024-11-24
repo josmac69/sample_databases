1fe34babc3e1396838384009b3621e85b74ab14c
Josef Machytka <josef.machytka@gmail.com>
2024-11-24 16:15:51 +0100

    updates from tests

--- python/import_mssql.py
+++ python/import_mssql.py
@@ -1,15 +1,18 @@
 import pandas as pd
 import duckdb
-import pyodbc
+# import pyodbc
 from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
+import sqlalchemy as sa
+from urllib.parse import quote_plus
 
 # Database connection parameters
 # server = 'localhost,1433'  # Replace with your server name
-server = 'localhost:1433'  # Replace with your server name
-database = 'master'  # Replace with your database name
-username = 'sa'  # Replace with your username
-password = 'TeST@+$-'  # Replace with your password
-table_name = 'MSreplication_options'  # Replace with the table name to import data from
+# server = 'localhost:1433'  # Replace with your server name
+# database = 'master'  # Replace with your database name
+# username = 'sa'  # Replace with your username
+# password = 'TeST@+$-'  # Replace with your password
+# table_name = 'dbo.MSreplication_options'  # Replace with the table name to import data from
 
 # Create a connection to the SQL Server
 # conn_str = f"DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}"
@@ -20,20 +23,36 @@ table_name = 'MSreplication_options'  # Replace with the table name to import da
 # dataframe = pd.read_sql(query, connection)
 
 # Create an SQLAlchemy engine
-# connection_string = f"mssql+pyodbc://{username}:{password}@{server}/{database}?driver=ODBC+Driver+17+for+SQL+Server"
-connection_string = f"mssql+pyodbc://{username}:{password}@{server}/{database}"
+# check with: odbcinst -q -d
+# connection_string = f"mssql+pyodbc://{username}:{password}@{server}/{database}?driver=ODBC+Driver+17+for+SQL+Server&TrustServerCertificate=yes"
+# connection_string = f"mssql+pyodbc://{username}:{password}@{server}/{database}?driver=SQL+Server+Native+Client+11.0"
+# connection_string = f"mssql+pyodbc://{username}:{password}@{server}/{database}"
+# print(connection_string)
+# engine = create_engine(connection_string)
+
+connection_string = "DRIVER={ODBC Driver 17 for SQL Server};SERVER=localhost,1433;DATABASE=master;UID=sa;PWD=TeST@+$-;"
+connection_url = URL.create("mssql+pyodbc", query={"odbc_connect": connection_string})
+engine_string = "mssql+pyodbc:///?odbc_connect={}".format(quote_plus(connection_string))
 print(connection_string)
-engine = create_engine(connection_string)
+print(connection_url)
+print(engine_string)
+# engine = create_engine(connection_url)
+engine = sa.create_engine(engine_string, pool_pre_ping=True, poolclass=None)
 print('engine created')
 # Read data from the SQL table
-query = f"SELECT * FROM {table_name}"
-dataframe = pd.read_sql_query(query, engine)
+query = "SELECT * FROM dbo.MSreplication_options"
+try:
+    with engine.begin() as conn:
+        dataframe = pd.read_sql(sa.text(query), conn)
+except Exception as error:
+    print(f"Error: {error}")
+    exit(1)
 
 # Verify the data
 print(dataframe.head())
 
 # Close the connection
-connection.close()
+# connection.close()
 
 conn = duckdb.connect('testdb.duckdb')
 # conn.execute("""ATTACH 'dbname=duckdb_test user=postgres password=postgres host=localhost port=5432'
